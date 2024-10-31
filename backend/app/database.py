b77e2a40a76608c74e4db6a2d5b090f06aeb1ace
mpaulagom <mariapaula.gomez@mercadolibre.com>
2024-10-30 21:06:12 -0300

    neo4j connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,13 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
+from neo4j import GraphDatabase
 import os
 
 
 load_dotenv()
 uri = os.getenv("MONGO_URI")
 
+# MongoDB database
 client = AsyncIOMotorClient(uri)
 db = client.proyecto_grado
 
@@ -13,3 +15,21 @@ print("connected to db")
 onto_collection = db.get_collection("ontologies")
 mapping_process_collection = db.get_collection("mapping_process")
 jsonschemas_collection = db.get_collection("json_schemas")
+
+# Neo4j database settings
+NEO_URI = os.getenv("NEO4J_URI")
+NEO_USER = os.getenv("NEO4J_USERNAME")
+NEO_PASS = os.getenv("NEO4J_PASSWORD")
+## Neo4j database
+class Neo4jConnection:
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+    
+    def close(self):
+        self._driver.close()
+    
+    def get_driver(self):
+        return self._driver
+
+# Instancia única de conexión
+neo4j_conn = Neo4jConnection(uri=NEO_URI, user=NEO_USER, password=NEO_PASS)
