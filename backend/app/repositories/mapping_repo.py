134ebb85404e58689dd69c2c31b6a09f85e9dc76
mpaulagom <mariapaula.gomez@mercadolibre.com>
2024-11-03 11:13:55 -0300

    get mappings by schema con otro scaffolding

---
+++ backend/app/repositories/mapping_repo.py
@@ -0,0 +1,9 @@
+from ..database import  mapping_process_collection
+
+
+
+async def find_mappings_by_schema(json_schema_id: str):
+    cursor = mapping_process_collection.find({'jsonSchemaId': json_schema_id})
+    mapping_process_docs = await cursor.to_list(length=None)
+    
+    return mapping_process_docs
