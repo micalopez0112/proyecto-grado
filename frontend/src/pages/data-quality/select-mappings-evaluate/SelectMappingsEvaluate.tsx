32fd98c2c6de59c9fd0a540f43afe2a182cb46f3
micaela <micaela@savingbucks.com>
2024-11-26 13:24:05 -0300

    fix infinite loop in data quality schema properties selection

--- frontend/src/pages/data-quality/select-mappings-evaluate/SelectMappingsEvaluate.tsx
+++ frontend/src/pages/data-quality/select-mappings-evaluate/SelectMappingsEvaluate.tsx
@@ -52,7 +52,6 @@ const SelectMappingsEvaluate = () => {
           if (response) {
             const { mapping_name, mapping, schema, ontology } = response.data;
             setMappingProcessId(mappingId);
-            console.log("mappingProcessId: ", mappingProcessId);
             setMappings(mapping);
             setJsonSchemaContext(schema);
             setcurrentOntologyId(ontology.ontology_id);
@@ -109,21 +108,18 @@ const SelectMappingsEvaluate = () => {
   const handleEvaluateSelectedMappings = async () => {
     setLoading(true);
     try {
-      // Perform the evaluation and store the response
       const response = await evaluateMapping(
         SYNTCTATIC_ACCURACY,
         mappingId,
         selectedMappings
       );
 
-      // Check if response is successful
       if (response) {
-        // Navigate to the results page, passing the response data
         navigate("/EvaluateMappings", {
           state: {
             selectedMappings,
             ruleId,
-            validationResults: response.data, // Pass evaluation results
+            validationResults: response.data,
           },
         });
       }
@@ -138,100 +134,47 @@ const SelectMappingsEvaluate = () => {
     properties: Record<string, JsonSchemaProperty>,
     parent: string
   ) => {
+    if (parent === "") {
+      return (
+        <div>
+          <div className="property-box" key={"rootObject"}>
+            <div className="json-elem disabled">
+              <strong>rootObject:</strong>
+            </div>
+            <div className="object-properties">
+              {renderProperties(properties, "rootObject")}
+            </div>
+          </div>
+        </div>
+      );
+    }
+
     return Object.entries(properties).map(([key, value]) => {
       let elementKey = parent ? `${parent}-${key}` : key;
-
       const fullKey = `${elementKey}_key#${value.type}`;
-
       const isMapped = !!mappings[fullKey];
-      // console.log(isMapped);
-      // console.log(fullKey);
-      // console.log({ mappings });
       const mappingInfo = isMapped ? mappings[fullKey] : null;
       const isActive = !!selectedMappings[fullKey];
 
-      if (parent === "") {
+      if (value.type === "object" && value.properties) {
+        const elementValue = elementKey + "_value";
+        const isMappedObjectValue = !!mappings[elementValue];
+        const mappingInfoValue = isMappedObjectValue
+          ? mappings[elementValue]
+          : null;
+
         return (
-          <div>
-            <div className="property-box" key={"rootObject"}>
-              <div className={`json-elem disabled`}>
-                <strong>rootObject:</strong>
-              </div>
-              <div className="object-properties">
-                {renderProperties(properties, "rootObject")}
-              </div>
+          <div className="property-box" key={key}>
+            <div className="json-elem disabled">
+              <strong>{key}:</strong> object
+            </div>
+            <div className="object-properties">
+              {renderProperties(value.properties, `${parent}-${key}`)}
             </div>
           </div>
         );
       }
-      if (value.type === "object" && value.properties) {
-        const elementValue = elementKey + "_value";
-        // console.log("elementValue: " + elementValue);
-        const isMappedObjectValue = !!mappings[elementKey + "_value"];
-        // console.log("isMappedObjectValue " + isMappedObjectValue);
-        const mappingInfoValue = isMappedObjectValue
-          ? mappings[elementValue]
-          : null;
-        const isActiveValue = !!selectedMappings[elementValue];
-        return (
-          <>
-            <div className={`property-box`} key={key}>
-              <div className={`json-elem disabled`}>
-                <strong>{key}:</strong> object
-              </div>
 
-              {
-                <div
-                  key={key + "_value"}
-                  className={`json-elem`}
-                  style={{
-                    marginLeft: "20px",
-                    backgroundColor: "#e3fae3",
-                    border: "6px solid #54c45e",
-                  }}
-                >
-                  <div className="mapping-box disabled">
-                    <strong>object</strong>
-                    {isMappedObjectValue && (
-                      <>
-                        <FaArrowRightLong />
-                        <span className="mapping-info">
-                          {mappingInfoValue?.map((map, index) => (
-                            <span key={index} title={map.iri}>
-                              {map.name}
-                            </span>
-                          ))}
-                        </span>
-                      </>
-                    )}
-                  </div>
-                  <div className="object-properties">
-                    {renderProperties(
-                      value.properties,
-                      parent ? parent + "-" + key : key
-                    )}
-                  </div>
-                </div>
-              }
-
-              {isMapped && (
-                <>
-                  <FaArrowRightLong />
-                  <span className="mapping-info">
-                    {mappingInfo?.map((map, index) => (
-                      <span key={index} title={map.iri}>
-                        {map.name}
-                      </span>
-                    ))}
-                  </span>
-                </>
-              )}
-            </div>
-          </>
-        );
-      }
-
-      // Handling for "array" type
       if (value.type === "array" && value.items) {
         return (
           <div className="property-box" key={key}>
@@ -245,15 +188,6 @@ const SelectMappingsEvaluate = () => {
             >
               <strong>{key}:</strong> array
             </div>
-            {isMapped && (
-              <span className="mapping-info">
-                {mappingInfo?.map((map, index) => (
-                  <span key={index} title={map.iri}>
-                    {map.name}
-                  </span>
-                ))}
-              </span>
-            )}
           </div>
         );
       }
@@ -270,18 +204,6 @@ const SelectMappingsEvaluate = () => {
           >
             <strong>{key}:</strong> {value.type}
           </div>
-          {isMapped && (
-            <>
-              <FaArrowRightLong />
-              <span className="mapping-info">
-                {mappingInfo?.map((map, index) => (
-                  <span key={index} title={map.iri}>
-                    {map.name}
-                  </span>
-                ))}
-              </span>
-            </>
-          )}
         </div>
       );
     });
@@ -296,7 +218,6 @@ const SelectMappingsEvaluate = () => {
           <h1 className="title-section">
             Select schema values to evaluate data
           </h1>
-
           <div className="select-mappings-container">
             {mappings && (
               <div className="mappings">
@@ -309,9 +230,8 @@ const SelectMappingsEvaluate = () => {
                 {jsonSchemaContext && (
                   <div className="json-schema-container">
                     <div className="json-schema">
-                      {jsonSchemaContext
-                        ? renderProperties(jsonSchemaContext.properties, "")
-                        : null}
+                      {jsonSchemaContext.properties &&
+                        renderProperties(jsonSchemaContext.properties, "")}
                     </div>
                   </div>
                 )}
