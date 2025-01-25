44d9514bc573e5941847b0254d03df5d637b1522
Facundo Nicolas Castro <fncastro@quanam.com>
2025-01-25 10:36:48 -0300

    Bug al hacer get_ontology.load(), fix con alambre

--- backend/app/services/ontology_service.py
+++ backend/app/services/ontology_service.py
@@ -1,7 +1,7 @@
 from fastapi import File, UploadFile, HTTPException, Form
 from typing import Optional, List, Any
 from app.models.ontology import OntologyDocument
-from owlready2 import get_ontology
+from owlready2 import get_ontology, default_world, close_world
 from app.repositories import ontology_repo
 import os
 toDirectory = "upload/ontologies"
@@ -9,6 +9,8 @@ async def save_ontology(type: str = Form(...), ontology_file: Optional[UploadFil
     try:
         ontology_id = ''
         ontology_data = {}
+        for onto in default_world.ontologies.values():
+            print("Ontology: ", onto.base_iri)
         if ontology_file and type == "FILE":
             if not os.path.exists(toDirectory):
                 os.makedirs(toDirectory)
@@ -28,8 +30,10 @@ async def save_ontology(type: str = Form(...), ontology_file: Optional[UploadFil
             else:
                 print("onto in collection")
                 ontology_id = str(onto_in_collection.id)
-            ontology = get_ontology(completePath).load()
-            print("Onto loaded")
+            ontology = get_ontology(completePath)
+            if not ontology.loaded:
+                print("Onto not loaded")
+                ontology.load()
         elif uri and type == "URI":
          # Manejo de la URI
             print("onto uri", uri)
@@ -40,7 +44,10 @@ async def save_ontology(type: str = Form(...), ontology_file: Optional[UploadFil
                 ontoDocu = OntologyDocument(type=type, uri=uri)
             else:
                 ontology_id = str(onto_in_collection.id)
-            ontology = get_ontology(str(uri)).load()
+            ontology = get_ontology(str(uri))
+            if not ontology.loaded:
+                print("Onto not loaded")
+                ontology.load()
         else:
             raise HTTPException(status_code=400, detail="No ontology FILE or URI provided")
         if(ontology_id == ''):
@@ -49,13 +56,11 @@ async def save_ontology(type: str = Form(...), ontology_file: Optional[UploadFil
             print("Inserted correctly - Ontology ID:", ontology_id)
         else:
             print("Ontology already exists - Ontology ID:", ontology_id)
-        print("##Ontology just before build_ontology_response##: ", ontology)
-        print("##")
         object_properties = ontology.object_properties()
-        print("##OP##:", object_properties)
         for prop in object_properties:
             print("##Object property ("+prop.name+") range: "+ str(prop.range)+" ##")
         print("##")
+        close_world(ontology)
         ontology_data = build_ontology_response(ontology, ontology_id)
         print("##return de la ontologia al hacer el upload (ver si hay object properties repetidas)##", ontology_data)
         return ontology_data
@@ -80,14 +85,32 @@ def build_ontology_response(ontology, onto_id):
     object_properties = list(ontology.object_properties())
     data_properties = list(ontology.data_properties())
     print("Ontology object properties:", object_properties)
+    ##Provitional solution to avoid duplicate object properties
+    seen_object_properties = set()
+    # Usamos set para evitar duplicados
+    response_object_properties = []
     for prop in object_properties:
-        print("Object property ("+prop.name+") range: "+ str(prop.range))
+        for range in prop.range:
+            key = (prop.name, prop.iri, range.name, range.iri)
+            if key not in seen_object_properties:
+                seen_object_properties.add(key)
+                response_object_properties.append({
+                    "name": prop.name,
+                    "iri": prop.iri,
+                    "range": {
+                        "name": range.name,
+                        "iri": range.iri
+                    }
+                })
+
+    #end of provitional solution
+    
     return {
         "ontology_id": onto_id,
         "ontoData": [{
             "data": [{
                 "classes": [{"name": cls.name, "iri": cls.iri} for cls in classes],
-                "object_properties": [{"name": prop.name, "iri": prop.iri,"range":{"name":range.name,"iri":range.iri}} for prop in object_properties for range in prop.range],
+                "object_properties": response_object_properties,
                 "data_properties": [{"name": prop.name, "iri": prop.iri} for prop in data_properties]
             }]
         }]
